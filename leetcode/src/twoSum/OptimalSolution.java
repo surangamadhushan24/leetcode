df60e4128b0ef907728b2b41eeb2a834524c84dd
suranga madhushan <150942338+surangamadhushan24@users.noreply.github.com>
2026-01-10 08:41:07 +0530

    add two sum souluttions

--- leetcode/src/twoSum/OptimalSolution.java
+++ leetcode/src/twoSum/OptimalSolution.java
@@ -1,2 +1,15 @@
-package twoSum;public class OptimalSolution {
+package twoSum;
+
+class OptimalSolution  {
+    public int countOdds(int low, int high) {
+        int total = (high - low) / 2;
+
+        // If either low or high is odd, add 1
+        if (low % 2 != 0 || high % 2 != 0) {
+            total += 1;
+        }
+
+        return total;
+    }
 }
+
--- leetcode/src/twoSum/Solution.java
+++ leetcode/src/twoSum/Solution.java
@@ -1,2 +1,12 @@
-package twoSum;public class Solution {
+package twoSum;
+
+class Solution {
+    public int countOdds(int low, int high) {
+        int count = 0;
+        for(int i=low;i<=high;i++){
+            if(i%2!=0){
+                count += 1; }
+        }
+        return count;
+    }
 }
