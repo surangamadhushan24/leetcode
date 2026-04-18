a1ccc8629e61d78b4b33e6b1bc9916daac92a5fa
suranga madhushan <150942338+surangamadhushan24@users.noreply.github.com>
2026-04-18 15:03:29 +0530

    implememnt pascal triangle

---
+++ leetcode/src/PascalTriangle1/Solution.java
@@ -0,0 +1,25 @@
+package PascalTriangle1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        if (numRows == 0) return Collections.emptyList();
+        List<List<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < numRows; ++i) {
+            List<Integer> sub = new ArrayList<>();
+            for (int j = 0; j <= i; ++j) {
+                if (j == 0 || j == i) {
+                    sub.add(1);
+                } else {
+                    List<Integer> upSub = list.get(i - 1);
+                    sub.add(upSub.get(j - 1) + upSub.get(j));
+                }
+            }
+            list.add(sub);
+        }
+        return list;
+    }
+}
