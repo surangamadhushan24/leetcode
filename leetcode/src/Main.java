b16186546c1266bbafc9fe0534371ec756e952d4
suranga madhushan <150942338+surangamadhushan24@users.noreply.github.com>
2026-04-22 22:54:18 +0530

    implement pascal triangle 2 problem

--- leetcode/src/Main.java
+++ leetcode/src/Main.java
@@ -2,7 +2,8 @@ import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-
-
+        String t = "two";
+        int y = Integer.parseInt(t);
+        System.out.println(y);
     }
 }
---
+++ leetcode/src/pascalTriangle2/Main.java
@@ -0,0 +1,8 @@
+package pascalTriangle2;
+
+public class Main {
+    public static void main(String[] args) {
+       Solution s1 = new Solution();
+        System.out.println(s1.getRow(5));
+    }
+}
